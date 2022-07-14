38b453bad16f9ec05cdadd78a0043e22c011e89d
JoabEGR <joabem99@hotmail.com>
2022-07-14 10:10:49 -0500

    Funcion ofuscar

--- BBVA/BBVA/BBVA.py
+++ BBVA/BBVA/BBVA.py
@@ -1,3 +1,18 @@
 
 
+def ofuscar(palabra):
+    textoofuscado = "";
+    if palabra!="":
+        longitud = len(palabra);
+        tresprimeros = palabra[0:3]
+        caracteresfaltantes = longitud - 3;
+        asteriscos = "";
+        i=0
+        while i<caracteresfaltantes:
+            asteriscos = asteriscos + "*";
+            i=i+1
+        textoofuscado=tresprimeros + asteriscos
+    return textoofuscado;
 
+
+print(ofuscar("Martinez"))
