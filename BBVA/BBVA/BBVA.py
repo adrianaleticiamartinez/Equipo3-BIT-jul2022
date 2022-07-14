d2ca9d6a57088c512c582069d137bddd6364ce54
DaniSA97 <109257135+DaniSA97@users.noreply.github.com>
2022-07-14 11:39:09 -0500

    Update BBVA.py

--- BBVA/BBVA/BBVA.py
+++ BBVA/BBVA/BBVA.py
@@ -7,6 +7,38 @@ insumos = pd.read_csv('./baseClientesHackaton2022.csv')
 usuarios = pd.read_csv('./baseUsuarios.csv')
 tipoUsuario = 0
 
+usuario_1 = "manager"
+usuario_2 = "validador"
+usuario_3 = "restringido"
+
+print("Bienvenido a BBVA")
+usuario = input("Ingrese usuario: ")
+passwword =input("Ingrese contraseña: ")
+
+if usuario ==usuario_1 or usuario == usuario_2 or usuario ==usuario_3:
+    print("Bienvenido ", usuario)
+    else: print("Usuario invalido")
+
+
+print("Clientes BBVA")
+print("¿Que tipo de usuario eres")
+if usuario ==usuario_1_ or usuario == usuario_2 or usuario ==usuario_3:
+    print(usuario)
+print("¿Que tipo de dato requieres")
+
+idCliente= input("Ingrese idCliente: ")
+nombre=input("Ingrese nombre: ")
+apellidoPaterno= input("Ingrese apellido paterno: ")
+apellidoMaterno= input("Ingrese apellido Materno: ")
+fechaNacimiento= input("Ingrese fecha de nacimiento: ")
+sexo= input("Ingrese tipo de sexo": )
+segmento= input("Ingrese segmento: ")
+nacionalidad= input("Ingrese nacionalidad:")
+rfc= input ("Ingrese RFC: ")
+tipoID= input("Ingrese tipo de identificación:")
+numeroID= input("Ingrese número de ID: ")
+cuenta= input("Ingrese número de cuenta:")
+email= input("Ingrese email del cliente: ")
 
 
 
