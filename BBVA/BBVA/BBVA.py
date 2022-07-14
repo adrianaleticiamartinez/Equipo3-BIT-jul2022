ecbaa4c18dcc51d3ca50f5ac7a0ee2f62d9d4759
HumbertoPerez <a01651926@tec.mx>
2022-07-14 11:04:40 -0500

    Update BBVA.py

--- BBVA/BBVA/BBVA.py
+++ BBVA/BBVA/BBVA.py
@@ -1,7 +1,9 @@
 import pandas as pd
-
+import sys
+import os
 
 insumos = pd.read_csv('./baseClientesHackaton2022.csv')
+usuarios = pd.read_csv('./baseUsuarios.csv')
 tipoUsuario = 0
 
 
@@ -17,5 +19,32 @@ def getNombre(_nombre, _apellidoP = ""):
                     print(insumos.loc[counter])
                     exit()
         counter += 1
+        
+def login(usuario, contraseña):
+    counter = 0
+    for i in usuarios.loc[:, "usuario"]:
+        if(i == usuario):
+            if(usuarios.loc[counter, "auth"] != contraseña):
+                tipoUsuario = ""
+                raise Exception("No es la contraseña correcta")
+            else:
+                tipoUsuario = usuarios.loc[counter, 'perfil']
+                
 
-
+def main():
+    global tipoUsusario
+    if(sys.argv[1] == ""):
+        #displayMessage()
+        print("pon usuario y contraseña")
+    else:
+        tipoUsusario = ""
+        login(sys.argv[1], sys.argv[2])
+        if(tipoUsusario == False):
+            exit()
+        else:
+            while (True):
+                nombre = input('Nombre de la busqueda')
+                apellido = input('Apellido de la busqueda')
+                getNombre(nombre, apellido)
+        
+main()
